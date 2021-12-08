33f4e6249a6d539eeff468fa6d0261bde036e7fa
ianlaporte <ianlaporte@gmail.com>
2021-12-07 23:32:50 -0500

    a13 added js 02

--- script/main.js
+++ script/main.js
@@ -1,17 +1,27 @@
+
+function timesTwo(params) {
+    return params * 2
+  };
+  
+  console.log(timesTwo(4));
+
+
+
+
 // The first, usual way, is by using the function keyword:
 // https://dmitripavlutin.com/differences-between-arrow-and-regular-functions/
 // Function declaration
-function greet(who) {
-  return `Hello, ${who}!`;
-}
+// function greet(who) {
+//   return `Hello, ${who}!`;
+// }
 // Function expression
-const greet = function(who) {
-  return `Hello, ${who}`;
-}
+// const greetme = function(who) {
+//   return `Hello, ${who}`;
+// }
 
 // The function declaration and function expression I'm going to reference as regular function.
 // The second way, available starting ES2015, is the arrow function syntax:
 
-const greet = (who) => {
-  return `Hello, ${who}!`;
-}
+// const greet = (who) => {
+//   return `Hello, ${who}!`;
+// }
